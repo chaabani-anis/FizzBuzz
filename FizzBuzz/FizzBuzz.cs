0a1047704458d2d0d5583a9a293a21083ced6f59
Anis <chaabani.anis@gmail.com>
2019-04-30 22:40:28 +0200

    Initial commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,10 @@
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string GetOutput(int i)
+        {
+            return "Fizz";
+        }
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTest
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void Should_return_Fizz_When_input_is_3()
+        {
+            var fizzBuzz = new FizzBuzz.FizzBuzz();
+            var output = fizzBuzz.GetOutput(3);
+            Assert.AreEqual("Fizz", output);
+        }
+    }
+}
